21ba0e582b4ddcc07bea371b457b19baeea9dc0f
Rohitha <rohitha.meda216@gmail.com>
2020-09-17 18:58:30 -0500

    added sql queries for group entity

--- Design Data/SQL QUERIES/group.sql
+++ Design Data/SQL QUERIES/group.sql
@@ -1,17 +1,13 @@
-DROP TABLE USERTBL;
+DROP TABLE Group_tbl;
 
-Create table userTBL(
-email varchar(30),
-password varchar(10),
-username varchar(20),
+Create table Group_tbl(
+Groupname varchar(50),
+creator varchar(10),
 createddate date, 
-lastaccessed timestamp default current_timestamp,
-ADMINcheck varchar(2),
-CONSTRAINT user_table_pk primary key(email));
+CONSTRAINT group_table_pk primary key(creator));
 
-Insert into userTBL values('johnsmith99@gmail.com', 'johnsmith@1', 'johnsmith99', CURRENT_DATE, CURRENT_TIMESTAMP, 'N');
-Insert into userTBL values('amyjackson12@gmail.com', 'jackson@2', 'amyjackson', CURRENT_DATE, CURRENT_TIMESTAMP, 'N');
-Insert into userTBL values('jadewilliam24@gmail.com', 'William!3', 'William', CURRENT_DATE, CURRENT_TIMESTAMP, 'N');
-Insert into userTBL values('sophiacat56@gmail.com', 'cathy@4', 'sophia', CURRENT_DATE, CURRENT_TIMESTAMP, 'Y');
-Insert into userTBL values('mysonjacob22@gmail.com', 'Jake#12', 'jacob', CURRENT_DATE, CURRENT_TIMESTAMP, 'N');
-
+Insert into Group_tbl values('Girls Group(14-18)Years', 'sophiacathie', CURRENT_DATE, CURRENT_TIMESTAMP);
+Insert into Group_tbl values('Boys Group(14-18)Years', 'Emilysmith', CURRENT_DATE, CURRENT_TIMESTAMP);
+Insert into Group_tbl values('Girls Group(19-30)Years', 'sophiacathie', CURRENT_DATE, CURRENT_TIMESTAMP);
+Insert into Group_tbl values('Boys Group(19-30)Years', 'Emilysmith', CURRENT_DATE, CURRENT_TIMESTAMP);
+Insert into Group_tbl values('Adult Group(30-50)Years', 'sophiacathie', CURRENT_DATE, CURRENT_TIMESTAMP);
