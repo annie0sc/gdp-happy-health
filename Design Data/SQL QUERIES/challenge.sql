0540619bac8d6f4a8546643bc6406c516c4a1e24
sowmya thogiti <sowmyathogiti27@gmail.com>
2020-09-17 18:56:43 -0500

    changes in queries

--- Design Data/SQL QUERIES/challenge.sql
+++ Design Data/SQL QUERIES/challenge.sql
@@ -1,17 +1,15 @@
 DROP TABLE dailylevel;
 
-Create table dailylevel(
-createddate date primary key,
-stepcount number(10),
-hoursofsleep number(10),
-fruitservings number(10), 
-vegetableservings number(10),
-totalcalories number(10),
-CONSTRAINT user_table_fk foreign key (createddate) references userTBL(createddate));
+Create table challenge(
+challengename varchar(20) primary key,
+designer varchar(20),
+createddate date,
+lastedited timestamp default current_timestamp, 
+CONSTRAINT challenge_table_fk foreign key (designer) references userTBL(username));
 
-Insert into dailylevel values(12-sep-20,3452,5,350,218,568);
-Insert into dailylevel values(18-sep-20,5872,3,222,123,568);
-Insert into dailylevel values(11-sep-20,1234,2,333,234,568);
-Insert into dailylevel values(10-sep-20,6798,4,444,345,568);
-Insert into dailylevel values(16-sep-20,7657,6,111,456,568);
+Insert into dailylevel values('step','sowmya',current_date,current_timestamp);
+Insert into dailylevel values('calorie','rohitha',current_date,current_timestamp);
+Insert into dailylevel values('diet','vignesh',current_date,current_timestamp);
+Insert into dailylevel values('workout','swaroopa',current_date,current_timestamp);
+Insert into dailylevel values('fat','bharath',current_date,current_timestamp);
 
--- Design Data/SQL QUERIES/dailylevel.sql
+++ Design Data/SQL QUERIES/dailylevel.sql
@@ -1,17 +1,16 @@
-DROP TABLE USERTBL;
+DROP TABLE dailylevel;
 
-Create table userTBL(
-email varchar(30),
-password varchar(10),
-username varchar(20),
-createddate date, 
-lastaccessed timestamp default current_timestamp,
-ADMINcheck varchar(2),
-CONSTRAINT user_table_pk primary key(email));
+Create table dailylevel(
+measurementdate date primary key,
+stepcount number(10),
+hoursofsleep number(10),
+fruitservings number(10), 
+vegetableservings number(10),
+totalcalories number(10),
+CONSTRAINT user_table_fk foreign key (measurementdate) references userTBL(createddate));
 
-Insert into userTBL values('johnsmith99@gmail.com', 'johnsmith@1', 'johnsmith99', CURRENT_DATE, CURRENT_TIMESTAMP, 'N');
-Insert into userTBL values('amyjackson12@gmail.com', 'jackson@2', 'amyjackson', CURRENT_DATE, CURRENT_TIMESTAMP, 'N');
-Insert into userTBL values('jadewilliam24@gmail.com', 'William!3', 'William', CURRENT_DATE, CURRENT_TIMESTAMP, 'N');
-Insert into userTBL values('sophiacat56@gmail.com', 'cathy@4', 'sophia', CURRENT_DATE, CURRENT_TIMESTAMP, 'Y');
-Insert into userTBL values('mysonjacob22@gmail.com', 'Jake#12', 'jacob', CURRENT_DATE, CURRENT_TIMESTAMP, 'N');
-
+Insert into dailylevel values(12-sep-20,3452,5,350,218,568);
+Insert into dailylevel values(18-sep-20,5872,3,222,123,568);
+Insert into dailylevel values(11-sep-20,1234,2,333,234,568);
+Insert into dailylevel values(10-sep-20,6798,4,444,345,568);
+Insert into dailylevel values(16-sep-20,7657,6,111,456,568);
