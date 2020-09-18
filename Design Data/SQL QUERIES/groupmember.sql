d83e7b7b187677ad0ecaf37829553505c64a29aa
Rohitha <rohitha.meda216@gmail.com>
2020-09-17 19:11:16 -0500

    created sql queries for groupmember entity

--- Design Data/SQL QUERIES/groupmember.sql
+++ Design Data/SQL QUERIES/groupmember.sql
@@ -1,17 +1,13 @@
-DROP TABLE USERTBL;
+DROP TABLE Group_member;
 
-Create table userTBL(
-email varchar(30),
-password varchar(10),
-username varchar(20),
-createddate date, 
-lastaccessed timestamp default current_timestamp,
-ADMINcheck varchar(2),
-CONSTRAINT user_table_pk primary key(email));
+Create table Group_member(
+Dateinvited date,
+Dateacceptedinvite date,
+Daterejectedinvite date,
+Dateleftgroup date, 
+CONSTRAINT groupmember_table_pk primary key(Dateinvited));
 
-Insert into userTBL values('johnsmith99@gmail.com', 'johnsmith@1', 'johnsmith99', CURRENT_DATE, CURRENT_TIMESTAMP, 'N');
-Insert into userTBL values('amyjackson12@gmail.com', 'jackson@2', 'amyjackson', CURRENT_DATE, CURRENT_TIMESTAMP, 'N');
-Insert into userTBL values('jadewilliam24@gmail.com', 'William!3', 'William', CURRENT_DATE, CURRENT_TIMESTAMP, 'N');
-Insert into userTBL values('sophiacat56@gmail.com', 'cathy@4', 'sophia', CURRENT_DATE, CURRENT_TIMESTAMP, 'Y');
-Insert into userTBL values('mysonjacob22@gmail.com', 'Jake#12', 'jacob', CURRENT_DATE, CURRENT_TIMESTAMP, 'N');
-
+Insert into Group_member values('2020/8/23', pending, pending, pending);
+Insert into Group_member values('2020/8/23', pending, pending);
+Insert into Group_member values('2020/8/20', '2020/8/23', Accepted, Accepted);
+Insert into Group_member values('2020/8/1', '2020/8/1', Accepted, '2020/8/15');
