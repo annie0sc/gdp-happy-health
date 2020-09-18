b7c38b45be5026eaf4484c945b29adb69545d2f8
Rohitha <rohitha.meda216@gmail.com>
2020-09-17 19:21:36 -0500

    created queries for target entity

--- Design Data/SQL QUERIES/target.sql
+++ Design Data/SQL QUERIES/target.sql
@@ -1,17 +1,13 @@
-DROP TABLE USERTBL;
+DROP TABLE Target_tbl;
 
-Create table userTBL(
-email varchar(30),
-password varchar(10),
-username varchar(20),
-createddate date, 
-lastaccessed timestamp default current_timestamp,
-ADMINcheck varchar(2),
-CONSTRAINT user_table_pk primary key(email));
+Create table Target_tbl(
+Targetname varchar(30),
+Targetdescription varchar(40),
+Targetnumericalgoal number(20),
+CONSTRAINT target_table_pk primary key(Targetname));
 
-Insert into userTBL values('johnsmith99@gmail.com', 'johnsmith@1', 'johnsmith99', CURRENT_DATE, CURRENT_TIMESTAMP, 'N');
-Insert into userTBL values('amyjackson12@gmail.com', 'jackson@2', 'amyjackson', CURRENT_DATE, CURRENT_TIMESTAMP, 'N');
-Insert into userTBL values('jadewilliam24@gmail.com', 'William!3', 'William', CURRENT_DATE, CURRENT_TIMESTAMP, 'N');
-Insert into userTBL values('sophiacat56@gmail.com', 'cathy@4', 'sophia', CURRENT_DATE, CURRENT_TIMESTAMP, 'Y');
-Insert into userTBL values('mysonjacob22@gmail.com', 'Jake#12', 'jacob', CURRENT_DATE, CURRENT_TIMESTAMP, 'N');
-
+Insert into Target_tbl values('3K Run', 'Takeupthischallengetocompleterunningfor3kilometers', 3);
+Insert into Target_tbl values('5K Run', 'Takeupthischallengetocompleterunningfor5kilometers', 5);
+Insert into Target_tbl values('10K Run', 'Takeupthischallengetocompleterunningfor10kilometers', 10);
+Insert into Target_tbl values('30days diet challenge', 'Takeupthischallengetocompletedietfor30days', 30);
+Insert into Target_tbl values('30daysworkoutchallenge', 'Takeupthisworkoutchallengetocompletein30days', 30);
