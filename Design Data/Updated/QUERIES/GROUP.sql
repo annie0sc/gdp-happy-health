3eabc6ae36b7bc2d504a3f0a44adae311c4f2aca
sowmya thogiti <sowmyathogiti27@gmail.com>
2020-09-27 22:19:20 -0500

    adding GROUP.sql in queries

---
+++ Design Data/Updated/QUERIES/GROUP.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS Sample_data_for_entities (
+    `Group_Name` VARCHAR(16) CHARACTER SET utf8,
+    `Creator` VARCHAR(11) CHARACTER SET utf8,
+    `Created_Date` DATETIME,
+    `Last_edited` VARCHAR(8) CHARACTER SET utf8
+);
+INSERT INTO Sample_data_for_entities VALUES
+    ('Avengers','William','2020-09-10 00:00:00','18:16:02'),
+    ('Champions','William','2020-09-11 00:00:00','15:12:23'),
+    ('Crew','William','2020-09-12 00:00:00','16:17:45'),
+    ('Bosses','William','2020-09-09 00:00:00','14:18:36'),
+    ('Force','William','2020-09-12 00:00:00','12:10:10'),
+    ('Hustle','Christopher','2020-09-14 00:00:00','10:10:10'),
+    ('Icons','Christopher','2020-09-08 00:00:00','11:11:11'),
+    ('Legends','Christopher','2020-09-09 00:00:00','12:12:12'),
+    ('Masters','Christopher','2020-09-10 00:00:00','13:13:13'),
+    ('Ninjas','Christopher','2020-09-11 00:00:00','14:14:14'),
+    ('Power','William','2020-09-12 00:00:00','15:15:15'),
+    ('Peak Performers','William','2020-09-13 00:00:00','16:17:18'),
+    ('Rebels','William','2020-09-15 00:00:00','17:18:19'),
+    ('Vikings','William','2020-09-09 00:00:00','18:19:20'),
+    ('Warriors','William','2020-09-11 00:00:00','19:20:21'),
+    ('United','William','2020-09-10 00:00:00','20:21:22'),
+    ('Squad','Christopher','2020-09-12 00:00:00','21:22:23'),
+    ('Rule Breakers','Christopher','2020-09-12 00:00:00','22:23:24'),
+    ('Monarchy','Christopher','2020-09-10 00:00:00','23:25:16'),
+    ('Best of the Best','Christopher','2020-09-08 00:00:00','16:18:19');
