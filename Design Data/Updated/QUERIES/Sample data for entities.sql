5677d9427f0967d5313b2299ae8589b1258d7fcf
vigneshwar6666 <vigneshwar6666@gmail.com>
2020-09-27 23:14:54 -0500

    Added CHALLENGE.sql in queries

---
+++ Design Data/Updated/QUERIES/Sample data for entities.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS Sample_data_for_entities (
+    `Challenge_Name` VARCHAR(9) CHARACTER SET utf8,
+    `Creator` VARCHAR(11) CHARACTER SET utf8,
+    `Created_Date` DATETIME,
+    `Last_Edited` VARCHAR(11) CHARACTER SET utf8
+);
+INSERT INTO Sample_data_for_entities VALUES
+    ('Steps','William','2020-09-21 00:00:00','13:11:10 PM'),
+    ('Calorie','William','2020-09-20 00:00:00','11:56:10'),
+    ('Weight','Christopher','2020-09-21 00:00:00','10:11:11'),
+    ('Diet','William','2020-09-21 00:00:00','12:12:12'),
+    ('Protein','Christopher','2020-09-22 00:00:00','13:13:13'),
+    ('Hydration','William','2020-09-22 00:00:00','15:14:14'),
+    ('Steps','William','2020-09-23 00:00:00','15:15:15'),
+    ('Calorie','Christopher','2020-09-22 00:00:00','16:17:18'),
+    ('Weight','Christopher','2020-09-20 00:00:00','17:18:19'),
+    ('Diet','Christopher','2020-09-22 00:00:00','18:19:20'),
+    ('Protein','William','2020-09-21 00:00:00','19:20:21'),
+    ('Hydration','Christopher','2020-09-22 00:00:00','20:21:22'),
+    ('Weight','Christopher','2020-09-23 00:00:00','21:22:23'),
+    ('Calorie','Christopher','2020-09-24 00:00:00','22:23:24'),
+    ('Protein','William','2020-09-21 00:00:00','23:25:16'),
+    ('Hydration','Christopher','2020-09-20 00:00:00','16:18:19'),
+    ('Diet','Christopher','2020-09-22 00:00:00','11:11:11'),
+    ('Steps','William','2020-09-20 00:00:00','12:12:12'),
+    ('Weight','William','2020-09-21 00:00:00','13:13:13'),
+    ('Calorie','William','2020-09-23 00:00:00','14:14:14');
